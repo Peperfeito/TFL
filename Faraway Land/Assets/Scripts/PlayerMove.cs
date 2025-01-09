b749ccbc5d17768659d484c5be4dbcceefb9382c
Peperfeito <gabrielcarvalhoesilva@gmail.com>
2025-01-09 18:26:22 -0300

    Teste 1
    sem flicflic

---
+++ Faraway Land/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private bool facingDireita = true;
+    private float movedirection;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+         if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+        if(movedirection > 0 && !facingDireita)
+        {
+            Flip();
+        }
+        else if(movedirection < 0 && facingDireita)
+        {
+            Flip();
+        }
+
+        movedirection = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(movedirection * moveSpeed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+
+    private void Flip()
+    {
+        facingDireita = !facingDireita;
+        transform.Rotate(0f, 180f,0f);
+    }
+}
