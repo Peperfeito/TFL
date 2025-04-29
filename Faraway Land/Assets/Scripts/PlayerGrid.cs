fe01c41a386f451a9a2426d387fadfca7b911a55
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2025-04-29 05:41:10 -0300

    grid movement oh yeahs

--- Faraway Land/Assets/Scripts/PlayerGrid.cs
+++ Faraway Land/Assets/Scripts/PlayerGrid.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,75 +8,51 @@ using UnityEngine.EventSystems;
 public class PlayerGrid : Player
 {
     public LayerMask colisores;
-   
-
 
     public Transform movePoint;
     private float horizontal;
     private float vertical;
-    [SerializeField] Player  player;
+    
+    private BoxCollider2D _boxCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
         
+        this._boxCollider = this.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        player.InputHandler();
+        InputHandler();
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        AndaGrid();
 
+        UpdateWaypointPosition();
+        MoveTowardsWaypoint();
     }
 
-    private void AndaGrid()
+    private void MoveTowardsWaypoint()
     {
-        bool moveflag = (transform.position - movePoint.position).magnitude > 0.2f;
-
-        Vector2 directionRayCast = new Vector2(horizontal, vertical);
-
-        if (!moveflag && Physics2D.Raycast(transform.position, directionRayCast, 1f, colisores))
-        {
-            Debug.DrawRay(transform.position, directionRayCast);
-            return;
-
-
-        }
-
+        if ((transform.position - movePoint.position).magnitude <= 0f) return;
         
-
-
-        if (!playerPodeSeMover) return;
-
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+    private void UpdateWaypointPosition()
+    {
+        if (Mathf.Abs(horizontal) >= 1f && Mathf.Abs(vertical) >= 1f) return;
+
+        RaycastHit2D hitInfo = Physics2D.BoxCast((Vector2)(this.transform.position) + this._boxCollider.offset, this._boxCollider.size, 0f, new Vector2(horizontal, vertical), 1, colisores);
+
+        if (!playerPodeSeMover || hitInfo.collider != null) return;
+
+        if ((transform.position - movePoint.position).magnitude <= 0f)
         {
-
-            if (Mathf.Abs(horizontal) == 1f)
-            {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontal, 0f, 0f), .2f, colisores))
-                {
-                    movePoint.position += new Vector3(horizontal, 0f, 0f);
-
-                }
-            }
-
-            if (Mathf.Abs(vertical) == 1f)
-            {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, vertical, 0f), .2f, colisores))
-                {
-                    movePoint.position += new Vector3(0f, vertical, 0f);
-
-                }
-            }
+            movePoint.position += new Vector3(horizontal, vertical, 0f);
         }
-
-       
-
     }
 
     
@@ -84,10 +61,6 @@ public class PlayerGrid : Player
     {
         if (collision.CompareTag("Porta"))
         {
-            Sidescroll.SetActive(true);
-            Grid.SetActive(false);
-            movePoint.position = Saida.position;
-            transform.position = Saida.position;
             FindObjectOfType<FadeEffect>().FadeOut();
             
             return;
